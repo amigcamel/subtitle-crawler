4282eec19315065ff2f7ab95b100b374e84987b4
amigcamel <amigcamel@gmail.com>
2018-01-11 15:51:53 +0800

    yield all movies' title and url

--- subcrawler/subcrawler/spiders/subhd.py
+++ subcrawler/subcrawler/spiders/subhd.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+"""Crawl subhd.com."""
 import logging
+import json
 
+from scrapy.http import FormRequest
 import scrapy
 
 logging.basicConfig(level=logging.DEBUG)
@@ -8,31 +11,53 @@ logger = logging.getLogger(__name__)
 
 
 class SubhdSpider(scrapy.Spider):
+    """A subhd.com crawler."""
+
     name = 'subhd'
     allowed_domains = ['subhd.com']
     start_urls = ['http://subhd.com/']
     _base_url = 'http://subhd.com'
 
     def parse(self, response):
+        """Parse all links."""
         selectors = response.css('a::attr(href)')
         for selector in selectors:
-            if selectors.re('/do0/\d+'):
+            if selector.re('/do0/\d+'):
                 url = f'{self._base_url}{selector.extract()}'
                 yield scrapy.Request(url=url, callback=self.parse_do0)
-            elif selector.re('/db0/\w+'):
-                url = f'{self._base_url}{selector.extract()}'
-                yield scrapy.Request(url=url, callback=self.parse)
             elif selector.re('/ar0/\d+'):
                 url = f'{self._base_url}{selector.extract()}'
                 yield scrapy.Request(url=url, callback=self.parse_ar0)
- 
-       
+            else:
+                url = f'{self._base_url}{selector.extract()}'
+                yield scrapy.Request(url=url, callback=self.parse)
+
     def parse_do0(self, response):
-        selectors = response.css('a[href^="/ar0/"]::attr(href)')
-        for selector in selectors:
-            url = f'{self._base_url}{selector.extract()}'
-            yield scrapy.Request(url=url, callback=self.parse_ar0)
+        """Parse "do0" pages."""
+        title_tran = response.css('.container h1 a::text').extract_first()
+        title_orig = response.css('.container h2 a::text').extract_first()
+        output = {
+            'url': response.url,
+            'title_orig': title_orig,
+            'title_tran': title_tran,
+        }
+        logger.debug(output)
+        yield output
+        # selectors = response.css('a[href^="/ar0/"]::attr(href)')
+        # for selector in selectors:
+        #     url = f'{self._base_url}{selector.extract()}'
+        #     yield scrapy.Request(url=url, callback=self.parse_ar0)
 
     def parse_ar0(self, response):
-        sid = response.css('#down::attr(sid)')
-        logging.info(f'sid: {sid}')
+        """Parse "ar0" pages."""
+        sid = response.css('#down::attr(sid)').extract_first()
+        formdata = {'sub_id': sid}
+        url = 'http://subhd.com/ajax/down_ajax'
+        yield FormRequest(
+            url=url, callback=self.parse_down_ajax, formdata=formdata)
+
+    def parse_down_ajax(self, response):
+        """Parse "ajax_down" page."""
+        data = json.loads(response.text)
+        if data['success']:
+            logging.info(data['url'])
